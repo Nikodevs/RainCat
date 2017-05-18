cbcd1a73873e3101dc1a959c1e4756604c247a6c
period2 <period2@admins-MacBook-Air.local>
2017-05-18 09:41:17 -0500

    Add deletion of Nodes, background color and umbrella

--- RainCat/BackgroundNodes.swift
+++ RainCat/BackgroundNodes.swift
@@ -21,5 +21,19 @@ public class BackgroundNode : SKNode {
         physicsBody?.restitution = 0.3
         physicsBody?.categoryBitMask = FloorCategory
         physicsBody?.contactTestBitMask = RainDropCategory
+        
+        let skyNode = SKShapeNode(rect: CGRect(origin: CGPoint(), size: size))
+        skyNode.fillColor = SKColor(red:0.38, green:0.60, blue:0.65, alpha:1.0)
+        skyNode.strokeColor = SKColor.clear
+        skyNode.zPosition = 0
+        
+        let groundSize = CGSize(width: size.width, height: size.height * 0.35)
+        let groundNode = SKShapeNode(rect: CGRect(origin: CGPoint(), size: groundSize))
+        groundNode.fillColor = SKColor(red:0.99, green:0.92, blue:0.55, alpha:1.0)
+        groundNode.strokeColor = SKColor.clear
+        groundNode.zPosition = 1
+        
+        addChild(skyNode)
+        addChild(groundNode)
     }
 }
--- RainCat/Scenes/GameScene.swift
+++ RainCat/Scenes/GameScene.swift
@@ -7,12 +7,13 @@
 
 import SpriteKit
 
-class GameScene: SKScene, SKSceneDelegate {
+class GameScene: SKScene, SKPhysicsContactDelegate {
 
   private var lastUpdateTime : TimeInterval = 0
   private var currentRainDropSpawnTime : TimeInterval = 0
   private var rainDropSpawnRate : TimeInterval = 0.5
   let raindropTexture = SKTexture(imageNamed: "rain_drop")
+    private let umbrellaNode = UmbrellaSprite.newInstance()
   private let backgroundNode = BackgroundNode()
     
   override func sceneDidLoad() {
@@ -24,22 +25,37 @@ class GameScene: SKScene, SKSceneDelegate {
     worldFrame.size.height += 200
     worldFrame.size.width += 200
     
+    self.physicsWorld.contactDelegate = self
+    
     self.physicsBody = SKPhysicsBody(edgeLoopFrom: worldFrame)
     self.physicsBody?.categoryBitMask = WorldCategory
     addChild(backgroundNode)
+    
+    umbrellaNode.updatePosition(point: CGPoint(x: frame.midX, y: frame.midY))
+    umbrellaNode.zPosition = 4
+    addChild(umbrellaNode)
   }
 
 
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-
-  }
-
-  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-
-  }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touchPoint = touches.first?.location(in: self)
+        
+        if let point = touchPoint {
+            umbrellaNode.setDestination(destination: point)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touchPoint = touches.first?.location(in: self)
+        
+        if let point = touchPoint {
+            umbrellaNode.setDestination(destination: point)
+        }
+    }
 
   override func update(_ currentTime: TimeInterval) {
     spawnRaindrop()
+    let dt = currentTime - self.lastUpdateTime
     // Called before each frame is rendered
 
     // Initialize _lastUpdateTime if it has not already been
@@ -47,16 +63,18 @@ class GameScene: SKScene, SKSceneDelegate {
       self.lastUpdateTime = currentTime
         
         // Update the spawn timer
-        currentRainDropSpawnTime += 0.5
+        currentRainDropSpawnTime += dt
         
         if currentRainDropSpawnTime > rainDropSpawnRate {
             currentRainDropSpawnTime = 0
             spawnRaindrop()
+            
+            umbrellaNode.update(deltaTime: dt)
         }
     }
 
     // Calculate time since last update
-    let dt = currentTime - self.lastUpdateTime
+    
 
     // Update the Spawn Timer
     currentRainDropSpawnTime += dt
@@ -75,7 +93,28 @@ class GameScene: SKScene, SKSceneDelegate {
         
         raindrop.physicsBody?.categoryBitMask = RainDropCategory
         raindrop.physicsBody?.contactTestBitMask = FloorCategory | WorldCategory
-        
+        raindrop.zPosition = 2
         addChild(raindrop)
     }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if (contact.bodyA.categoryBitMask == RainDropCategory) {
+            contact.bodyA.node?.physicsBody?.collisionBitMask = 0
+            contact.bodyA.node?.physicsBody?.categoryBitMask = 0
+        } else if (contact.bodyB.categoryBitMask == RainDropCategory) {
+            contact.bodyB.node?.physicsBody?.collisionBitMask = 0
+            contact.bodyB.node?.physicsBody?.categoryBitMask = 0
+        }
+        
+        if contact.bodyA.categoryBitMask == WorldCategory {
+            contact.bodyB.node?.removeFromParent()
+            contact.bodyB.node?.physicsBody = nil
+            contact.bodyB.node?.removeAllActions()
+        } else if contact.bodyB.categoryBitMask == WorldCategory {
+            contact.bodyA.node?.removeFromParent()
+            contact.bodyA.node?.physicsBody = nil
+            contact.bodyA.node?.removeAllActions()
+        }
+    }
+    
 }
